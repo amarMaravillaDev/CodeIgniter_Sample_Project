38bef448dd850c0568e6e9d98343c8c55ce3e88c
Amar Maravilla <amar@clearmindai.com>
2024-01-08 18:47:28 +0800

    Updating a Controller
    Updating a Controller, the Welcome Controller

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -1,13 +1,18 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Welcome extends CI_Controller {
-	public function index() {
+class Welcome extends CI_Controller 
+{
+	public function index() 
+	{
 		$this->load->view('welcome_message');
 	}
 
 	// Added by Amar Maravilla
-	public function about() {
-		echo "I'm About Page.";
+	public function about() 
+	{
+		// echo "I'm About Page.";
+
+		$this->load->view('about');
 	}
 }
