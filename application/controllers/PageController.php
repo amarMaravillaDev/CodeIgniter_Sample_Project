cff7753a97428c2baf8b0a8600ed7c56317fec4f
Amar Maravilla <amar@clearmindai.com>
2024-01-09 18:31:57 +0800

    Update and Adding a new Function
    Update the PageController and Adding a new Function in the Page Controller

--- application/controllers/PageController.php
+++ application/controllers/PageController.php
@@ -2,15 +2,22 @@
 // Added by Amar Maravilla
 defined('BASEPATH') or exit('No direct script access allowed');
 
-class PageController extends CI_Controller
-{  
-  public function __construct()
-  {
+class PageController extends CI_Controller {  
+  public function __construct() {
     parent::__construct();
   }
 
-  public function index()
-  {
-    echo "This is Page Controller.";
+  public function index() {
+    echo "This is Home Page.";
+  }
+
+  public function aboutus() {
+    echo "This is About Us Page.";
+  }
+
+  public function blog($blogID = '') {
+    echo $blogID;
+    
+    $this->load->view('blog');
   }
 }
