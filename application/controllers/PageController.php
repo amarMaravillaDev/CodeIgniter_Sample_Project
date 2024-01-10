ada71e926c70fcd3d9f830d18ea35c2c80cd71ae
Amar Maravilla <amar@clearmindai.com>
2024-01-10 21:24:32 +0800

    Updating a Controller
    Updating a Controller, the PageController

--- application/controllers/PageController.php
+++ application/controllers/PageController.php
@@ -1,23 +1,24 @@
-<?php
+<?php 
 // Added by Amar Maravilla
-defined('BASEPATH') or exit('No direct script access allowed');
+defined('BASEPATH') OR exit('No direct script access allowed');
 
-class PageController extends CI_Controller {  
-  public function __construct() {
-    parent::__construct();
-  }
+class PageController extends CI_Controller {
+    public function __construct()
+    {
+        parent::__construct();
+    }
 
-  public function index() {
-    echo "This is Home Page.";
-  }
+    public function index() {
+        echo "This is Home Page.";
+    }
 
-  public function aboutus() {
-    echo "This is About Us Page.";
-  }
+    public function aboutus() {
+        echo "This is About Us Page.";
+    }
 
-  public function blog($blogID = '') {
-    echo $blogID;
-    
-    $this->load->view('blog');
-  }
+    public function blog($blogID = '') {
+        echo $blogID;
+
+        $this->load->view('blog');
+    }
 }
