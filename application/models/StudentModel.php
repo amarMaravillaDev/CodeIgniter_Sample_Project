cfd8fbd16fa857c7e31fe15b69515b9099fc7c5b
Amar Maravilla <amar@clearmindai.com>
2024-01-10 21:25:21 +0800

    Adding a new Model
    Adding a new Model, the StudentModel

---
+++ application/models/StudentModel.php
@@ -0,0 +1,24 @@
+<?php 
+// Added by Amar Maravilla
+defined('BASEPATH') OR exit('No direct script access allowed');
+                        
+class StudentModel extends CI_Model {
+	public function studentData() {
+		$studentClass = $this->studentClass();
+		
+		return $name = "Amar His Class is: " . $studentClass;
+	}  
+	
+	private function studentClass() {
+		return $class = "BSCS";
+	}  
+
+	public function students($ID) {
+		if($ID == '1') {
+			return $result = "This is User 1";
+		} 
+		else if($ID == '2') {
+			return $result = "This is User 2";
+		}
+	}
+}
