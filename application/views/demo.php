57f10c3c24317b0abc640b320141d42659980642
Amar Maravilla <amar@clearmindai.com>
2024-01-11 23:52:22 +0800

    Adding a new View
    Adding a new View, the demo

---
+++ application/views/demo.php
@@ -0,0 +1,18 @@
+<?php 
+// Added by Amar Maravilla
+defined('BASEPATH') OR exit('No direct script access allowed'); 
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h3> <?php echo $title; ?> </h3>
+    <h1> Welcome to CodeIgniter Crash Course For Beginners. </h1>
+    <h4> <?= $body; ?> </h4>
+</body>
+</html>
