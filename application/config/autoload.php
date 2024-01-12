0891450b70c855be4365dc188e2e0fbcdcfb8115
Amar Maravilla <amar@clearmindai.com>
2024-01-12 19:11:23 +0800

    Adding new Library
    Adding new Library, the database

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -58,7 +58,8 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array();
+
+$autoload['libraries'] = array('database');
 
 /*
 | -------------------------------------------------------------------
@@ -79,6 +80,7 @@ $autoload['libraries'] = array();
 |	$autoload['drivers'] = array('cache' => 'cch');
 |
 */
+
 $autoload['drivers'] = array();
 
 /*
@@ -89,6 +91,7 @@ $autoload['drivers'] = array();
 |
 |	$autoload['helper'] = array('url', 'file');
 */
+
 $autoload['helper'] = array();
 
 /*
@@ -103,6 +106,7 @@ $autoload['helper'] = array();
 | config files.  Otherwise, leave it blank.
 |
 */
+
 $autoload['config'] = array();
 
 /*
@@ -117,6 +121,7 @@ $autoload['config'] = array();
 | "codeigniter_lang.php" would be referenced as array('codeigniter');
 |
 */
+
 $autoload['language'] = array();
 
 /*
@@ -132,4 +137,5 @@ $autoload['language'] = array();
 |
 |	$autoload['model'] = array('first_model' => 'first');
 */
+
 $autoload['model'] = array();
