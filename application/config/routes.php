1204262376f6df3266958e550fbf06d0e1ecb6d0
Amar Maravilla <amar@clearmindai.com>
2024-01-14 00:45:18 +0800

    Adding new Routes
    Adding new Routes in routes Config

--- application/config/routes.php
+++ application/config/routes.php
@@ -1,15 +1,19 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+    defined('BASEPATH') OR exit('No direct script access allowed');
 
-$route['default_controller'] = 'Welcome';
-$route['404_override'] = '';
-$route['translate_uri_dashes'] = FALSE;
+    $route['default_controller'] = 'Welcome';
+    $route['404_override'] = '';
+    $route['translate_uri_dashes'] = FALSE;
 
-// Added by Amar Maravilla
-// Welcome Routes
-$route['about'] = 'Welcome/about';
+    // Added by Amar Maravilla
+    // Welcome Routes
+    $route['about'] = 'Welcome/about';
 
-// PageController Routes
-$route['home'] = 'PageController';
-$route['aboutus'] = 'PageController/aboutus';
-$route['blog/(:any)'] = 'PageController/blog/$1';
+    // PageController Routes
+    $route['home'] = 'PageController';
+    $route['aboutus'] = 'PageController/aboutus';
+    $route['blog/(:any)'] = 'PageController/blog/$1';
+
+    // Employee Routes
+    $route['employee'] = 'employee/EmployeeController';
+    $route['employee/create'] = 'employee/EmployeeController/create';
