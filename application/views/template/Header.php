3df4b13cc1af1dcf69ca5b642d736967c95c82d0
Amar Maravilla <amar@clearmindai.com>
2024-01-14 00:48:53 +0800

    Adding a new View Template
    Adding a new View Template, the Header View Template

---
+++ application/views/template/Header.php
@@ -0,0 +1,19 @@
+<?php 
+    // Amar
+    defined('BASEPATH') OR exit('No direct script access allowed'); 
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+
+    <title>Employee Page</title>
+    </head>
+
+    <body>
