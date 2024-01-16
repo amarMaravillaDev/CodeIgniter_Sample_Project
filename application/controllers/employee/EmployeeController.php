3c20ff34269ac854d92f1fcda8edc146d4da31ff
Amar Maravilla <amar@clearmindai.com>
2024-01-16 20:53:39 +0800

    Adding a new Function
    Adding a new Function, the store Function in EmployeeController Controller

--- application/controllers/employee/EmployeeController.php
+++ application/controllers/employee/EmployeeController.php
@@ -19,4 +19,34 @@
             $this->load->view('employee/CreateEmployeeView');
             $this->load->view('template/Footer');
         }
+
+        public function store() {
+            $this->form_validation->set_rules('employeeFirstName', 'First Name', 'required');
+            $this->form_validation->set_rules('employeeLastName', 'Last Name', 'required');
+            $this->form_validation->set_rules('employeePhoneNumber', 'Phone Number', 'required');
+            $this->form_validation->set_rules('employeeEmailAddress', 'Email Address', 'required');
+
+            if($this->form_validation->run()) {
+                $employeeData = array(
+                    'FIRST_NAME' => $this->input->post('employeeFirstName'),
+                    'LAST_NAME' => $this->input->post('employeeLastName'),
+                    'PHONE_NUMBER' => $this->input->post('employeePhoneNumber'),
+                    'EMAIL_ADDRESS' => $this->input->post('employeeEmailAddress')
+                );
+
+                $this->load->model('EmployeeModel', 'Employee');
+
+                $this->Employee->insertEmployee($employeeData);
+
+                redirect(base_url('employee'));
+            }
+            else {
+                $this->create();
+                
+                // redirect(base_url('employee/add'));
+            }
+
+            // var_dump($employeeData);
+            // echo json_encode($employeeDat);
+        }
     }
