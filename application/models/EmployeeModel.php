c4124610c9cf513d2a7ccf9c221c30700f9481ca
Amar Maravilla <amar@clearmindai.com>
2024-01-16 20:49:58 +0800

    Adding a new Model
    Adding a new Model, the EmployeeModel Model

---
+++ application/models/EmployeeModel.php
@@ -0,0 +1,8 @@
+<?php 
+    defined('BASEPATH') OR exit('No direct script access allowed');
+                            
+    class EmployeeModel extends CI_Model {
+        public function insertEmployee($employeeData) {
+            return $this->db->insert('EMPLOYEE_DATA', $employeeData);
+        }                  
+    }
