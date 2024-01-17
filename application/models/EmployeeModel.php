176a383c2302785a04f02a26264256dd322e3498
Amar Maravilla <amar@clearmindai.com>
2024-01-17 22:32:44 +0800

    Adding a new Function
    Adding a new Function, the getEmployee Function in EmployeeModel Model

--- application/models/EmployeeModel.php
+++ application/models/EmployeeModel.php
@@ -2,6 +2,11 @@
     defined('BASEPATH') OR exit('No direct script access allowed');
                             
     class EmployeeModel extends CI_Model {
+        public function getEmployee() {
+            $query = $this->db->get('EMPLOYEE_DATA');
+
+            return $query->result();
+        }
         public function insertEmployee($employeeData) {
             return $this->db->insert('EMPLOYEE_DATA', $employeeData);
         }                  
