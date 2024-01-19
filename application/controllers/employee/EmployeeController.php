d1cf7c30860b85ce778528264d5e7d5d1d687904
Amar Maravilla <amar@clearmindai.com>
2024-01-19 22:08:32 +0800

    Adding new Functions
    Adding new Functions, the update and delete Function in EmployeeController Controller

--- application/controllers/employee/EmployeeController.php
+++ application/controllers/employee/EmployeeController.php
@@ -65,4 +65,39 @@
             $this->load->view('employee/EditEmployeeView', $employeeData);
             $this->load->view('template/Footer');
         }
+
+        public function update($employeeID) {
+            $this->form_validation->set_rules('employeeFirstName', 'First Name', 'required');
+            $this->form_validation->set_rules('employeeLastName', 'Last Name', 'required');
+            $this->form_validation->set_rules('employeePhoneNumber', 'Phone Number', 'required');
+            $this->form_validation->set_rules('employeeEmailAddress', 'Email Address', 'required');
+
+            if($this->form_validation->run()) {
+                $employeeData = array(
+                    'FIRST_NAME' => $this->input->post('employeeFirstName'),
+                    'LAST_NAME' => $this->input->post('employeeLastName'),
+                    'PHONE_NUMBER' => $this->input->post('employeePhoneNumber'),
+                    'EMAIL_ADDRESS' => $this->input->post('employeeEmailAddress')
+                );
+    
+                $this->load->model('EmployeeModel', 'Employee');
+    
+                $employeeData['employeeData'] = $this->Employee->updateEmployee($employeeData, $employeeID);
+                
+                redirect(base_url('employee'));
+            }
+            else {
+                // redirect(base_url('employee'));
+
+                $this->edit($employeeID);
+            }
+        }
+
+        public function delete($employeeID) {
+            $this->load->model('EmployeeModel', 'Employee');
+            
+            $this->Employee->deleteEmployee($employeeID);
+
+            redirect(base_url('employee'));
+        }
     }
