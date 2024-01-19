a26447048de574e3a9e29bc9efd68b766df8fdf3
Amar Maravilla <amar@clearmindai.com>
2024-01-19 22:11:32 +0800

    Adding a new Asset
    Adding a new Asset, the EmployeeScript Asset

---
+++ assets/EmployeeScript.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+    $(".deleteEmployee").on('click', function(event) {
+        event.preventDefault();
+
+        let confirmDialog = confirm("Are You Sure You Want to Delete this Record?");
+
+        if(confirmDialog) {
+            let employeeID =  $(this).val();
+
+            $.ajax({
+                type: "DELETE",
+                url: "employee/delete/" + employeeID,
+                success: function (response) {
+                    alert("Employee Record Deleted Successfully.");
+
+                    window.location.reload();
+                }
+            });
+        }
+    });
+});
