30a24c1546a0935f5f1ad29bc908fe8c88902406
Amar Maravilla <amar@clearmindai.com>
2024-01-20 21:22:42 +0800

    Updating a Config
    Updating a Config, the autoload Config

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -59,7 +59,7 @@ $autoload['packages'] = array();
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
 
-$autoload['libraries'] = array('database', 'form_validation');
+$autoload['libraries'] = array('database', 'form_validation', 'session');
 
 /*
 | -------------------------------------------------------------------
