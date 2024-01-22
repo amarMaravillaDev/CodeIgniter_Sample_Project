6a239d4f505508952a2c55620c66cb73ee7b70c6
Amar Maravilla <amar@clearmindai.com>
2024-01-22 20:27:05 +0800

    Updating a Controller
    Updating a Controller, the EmployeeController Controller

--- application/controllers/employee/EmployeeController.php
+++ application/controllers/employee/EmployeeController.php
@@ -3,7 +3,6 @@
     defined('BASEPATH') OR exit('No direct script access allowed');
 
     class EmployeeController extends CI_Controller {
-
         public function __construct() {
             parent::__construct();
         }
