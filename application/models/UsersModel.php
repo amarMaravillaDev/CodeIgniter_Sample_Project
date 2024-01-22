07192cc108613c0a619d448da3c2d275936dac94
Amar Maravilla <amar@clearmindai.com>
2024-01-22 20:28:07 +0800

    Updating a Model
    Updating a Model, the UsersModel Model

--- application/models/UsersModel.php
+++ application/models/UsersModel.php
@@ -5,7 +5,24 @@
     class UsersModel extends CI_Model {
         public function registerUser($usersData){
             return $this->db->insert('USERS', $usersData);                
-        }               
+        }           
+        
+        public function loginUser($usersData){
+            $this->db->select('*');
+            $this->db->where('EMAIL_ADDRESS', $usersData['EMAIL_ADDRESS']);
+            $this->db->where('PASSWORD', $usersData['PASSWORD']);
+            $this->db->from('USERS');
+            $this->db->limit(1);
+
+            $query = $this->db->get();
+
+            if($query->num_rows() == 1){
+                return $query->row();
+            }
+            else {
+                return FALSE;
+            }
+        }
     }
     
                         
