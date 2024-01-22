8a1b9b1efbe454670c0d098c088f1a9f6a106656
Amar Maravilla <amar@clearmindai.com>
2024-01-22 20:27:41 +0800

    Adding a new Controller
    Adding a new Controller, the UsersController Controller

---
+++ application/controllers/users/UsersController.php
@@ -0,0 +1,15 @@
+<?php 
+    // Amar
+    defined('BASEPATH') OR exit('No direct script access allowed');
+
+    class UsersController extends CI_Controller { 
+        public function __construct() {
+            parent::__construct();
+        }
+
+        public function index() {
+            $this->load->view('template/Header');
+            $this->load->view('users/UsersView');
+            $this->load->view('template/Footer');
+        }
+    }
