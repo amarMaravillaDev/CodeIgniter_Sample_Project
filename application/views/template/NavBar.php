a9281896e95bccf45996fd744503383b70365e2c
Amar Maravilla <amar@clearmindai.com>
2024-01-24 19:36:25 +0800

    Updating a View Template
    Updating a View Template, the NavBar View Template

--- application/views/template/NavBar.php
+++ application/views/template/NavBar.php
@@ -2,24 +2,27 @@
     defined('BASEPATH') OR exit('No direct script access allowed'); 
 ?>
 
-<nav class="navbar navbar-expand-lg sticky-top navbar-dark bg-dark">
+<nav class="navbar navbar-expand-lg sticky-top navbar-dark bg-dark px-5">
     <a class="navbar-brand" href="#">Code Igniter Sample Project</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
 
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+    <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="<?= base_url('users'); ?>">User Page</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#">User Page</a>
+                <a class="nav-link" href="<?= base_url('admins'); ?>">Admin Page</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#">Admin Page</a>
+                <a class="nav-link" href="<?= base_url('login'); ?>">Login</a>
             </li>
-            <li class="nav-item dropdown">
+            <li class="nav-item">
+                <a class="nav-link" href="<?= base_url('register'); ?>">Register</a>
+            </li>
+            <!-- <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-expanded="false">
                     Dropdown
                 </a>
@@ -29,7 +32,7 @@
                     <div class="dropdown-divider"></div>
                     <a class="dropdown-item" href="#">Something else here</a>
                 </div>
-            </li>
+            </li> -->
         </ul>
     </div>
 </nav>
