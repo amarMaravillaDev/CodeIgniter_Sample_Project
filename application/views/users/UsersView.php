ee22cb549a2c2e2f97e048dd371c95fd31100fcd
Amar Maravilla <amar@clearmindai.com>
2024-01-25 22:54:07 +0800

    Updating a View
    Updating a View, the UsersView View

--- application/views/users/UsersView.php
+++ application/views/users/UsersView.php
@@ -5,11 +5,26 @@
 <div class="py-5">
     <div class="container">
         <div class="row justify-content-center">
-            <?php if($this->session->flashdata('loginSuccess')) { ?>
-            <div class="alert alert-success">
-                <?= $this->session->flashdata('loginSuccess') ?>
+            <div class="col-md-12">
+                
+                <?php if($this->session->flashdata('loginSuccess')) { ?>
+                    <div class="alert alert-success">
+                        <?= $this->session->flashdata('loginSuccess') ?>
+                    </div>
+                <?php } ?>
+
+                <div class="card">
+                    <div class="card-header">
+                        <h5>User's Page</h5>
+                    </div>
+                    <div class="card-body">
+                        <h6>This is User's Page</h6>
+                        <h5>First Name: <?= $this->session->userdata('usersDetails')['FIRST_NAME']; ?></h5>
+                        <h5>Last Name: <?= $this->session->userdata('usersDetails')['LAST_NAME']; ?></h5>
+                        <h5>Email Address: <?= $this->session->userdata('usersDetails')['EMAIL_ADDRESS']; ?></h5>
+                    </div>
+                </div>
             </div>
-            <?php } ?>
         </div>
     </div>
 </div>
