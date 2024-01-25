3e71a66b840ceb0a17accc8540f48ad859e50bce
Amar Maravilla <amar@clearmindai.com>
2024-01-25 22:53:44 +0800

    Updating a View Template
    Updating a View Template, the NavBar View Template

--- application/views/template/NavBar.php
+++ application/views/template/NavBar.php
@@ -2,37 +2,44 @@
     defined('BASEPATH') OR exit('No direct script access allowed'); 
 ?>
 
-<nav class="navbar navbar-expand-lg sticky-top navbar-dark bg-dark px-5">
-    <a class="navbar-brand" href="#">Code Igniter Sample Project</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+<nav class="navbar navbar-expand-lg sticky-top navbar-dark bg-dark">
+    <div class="container">
+        <a class="navbar-brand" href="#">Code Igniter Sample Project</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
-    </button>
+        </button>
 
-    <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
-        <ul class="navbar-nav">
-            <li class="nav-item">
-                <a class="nav-link" href="<?= base_url('users'); ?>">User Page</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="<?= base_url('admins'); ?>">Admin Page</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="<?= base_url('login'); ?>">Login</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="<?= base_url('register'); ?>">Register</a>
-            </li>
-            <!-- <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-expanded="false">
-                    Dropdown
-                </a>
-                <div class="dropdown-menu">
-                    <a class="dropdown-item" href="#">Action</a>
-                    <a class="dropdown-item" href="#">Another action</a>
-                    <div class="dropdown-divider"></div>
-                    <a class="dropdown-item" href="#">Something else here</a>
-                </div>
-            </li> -->
-        </ul>
+        <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="<?= base_url('users'); ?>">User Page</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="<?= base_url('admins'); ?>">Admin Page</a>
+                </li>
+
+                <?php if(!$this->session->has_userdata('client')) { ?>
+                    <li class="nav-item">
+                        <a class="nav-link" href="<?= base_url('login'); ?>">Login</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="<?= base_url('register'); ?>">Register</a>
+                    </li>
+                <?php } ?>
+
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-expanded="false">
+                        <?= $this->session->userdata('usersDetails')['FIRST_NAME']; ?>
+                        <?= $this->session->userdata('usersDetails')['LAST_NAME']; ?>
+                    </a>
+                    <div class="dropdown-menu">
+                        <!-- <a class="dropdown-item" href="#">Action</a>
+                        <a class="dropdown-item" href="#">Another action</a>
+                        <div class="dropdown-divider"></div> -->
+                        <a class="dropdown-item" href="#">Log Out</a>
+                    </div>
+                </li>
+            </ul>
+        </div>
     </div>
 </nav>
