703fc1e599e9ebc710d8bf210722d5c12fc0dc04
Amar Maravilla <amar@clearmindai.com>
2024-01-25 22:51:39 +0800

    Updating a Controller
    Updating a Controller, the UsersController Controller

--- application/controllers/users/UsersController.php
+++ application/controllers/users/UsersController.php
@@ -5,6 +5,8 @@
     class UsersController extends CI_Controller { 
         public function __construct() {
             parent::__construct();
+
+            $this->load->model('AuthenticationModel', 'Authenticate');
         }
 
         public function index() {
