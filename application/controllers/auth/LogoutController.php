d365034928c335558b306a6c9318b7edc272a0d5
Amar Maravilla <amar@clearmindai.com>
2024-01-26 23:49:02 +0800

    Adding a new Controller
    Adding a new Controller, the LogoutController Controller

---
+++ application/controllers/auth/LogoutController.php
@@ -0,0 +1,24 @@
+<?php 
+    // Amar
+    defined('BASEPATH') OR exit('No direct script access allowed');
+
+    class LogoutController extends CI_Controller {
+        public function __construct() {
+            parent::__construct();
+
+            $this->load->model('AuthenticationModel', 'Authenticate');
+        }
+
+        public function index() {
+
+        }
+
+        public function logout() {
+            $this->session->unset_userdata('client');
+            $this->session->unset_userdata('usersDetails');
+
+            $this->session->set_flashdata('logoutSuccess', 'You are Logged Out Successfully.');
+
+            return redirect('login');
+        }
+    }
