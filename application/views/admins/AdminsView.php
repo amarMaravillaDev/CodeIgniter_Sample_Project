b3b76a5293b765c72e51f38b5547a621bac66742
Amar Maravilla <amar@clearmindai.com>
2024-01-26 23:49:33 +0800

    Updating a View
    Updating a View, the AdminsView View

--- application/views/admins/AdminsView.php
+++ application/views/admins/AdminsView.php
@@ -6,7 +6,7 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-12">
-                
+
                 <?php if($this->session->flashdata('loginSuccess')) { ?>
                     <div class="alert alert-success">
                         <?= $this->session->flashdata('loginSuccess') ?>
@@ -15,13 +15,13 @@
 
                 <div class="card">
                     <div class="card-header">
-                        <h5>Admin's Page</h5>
+                        <h5 class="my-2">Admin's Page</h5>
                     </div>
                     <div class="card-body">
                         <h6>This is Admin's Page</h6>
                         <h5>First Name: <?= $this->session->userdata('usersDetails')['FIRST_NAME']; ?></h5>
                         <h5>Last Name: <?= $this->session->userdata('usersDetails')['LAST_NAME']; ?></h5>
-                        <h5>Email Address: <?= $this->session->userdata('usersDetails')['EMAIL_ADDRESS']; ?></h5>
+                        <h5 class="m-0">Email Address: <?= $this->session->userdata('usersDetails')['EMAIL_ADDRESS']; ?></h5>
                     </div>
                 </div>
             </div>
