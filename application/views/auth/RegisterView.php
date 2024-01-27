04c674f194598d59b5da5e6a6a143817174076d3
Amar Maravilla <amar@clearmindai.com>
2024-01-27 21:23:40 +0800

    Updating a View
    Updating a View, the RegisterView View

--- application/views/auth/RegisterView.php
+++ application/views/auth/RegisterView.php
@@ -5,63 +5,71 @@
 
 <div class="py-5">
     <div class="container"></div>
-        <div class="row justify-content-center">
-            <div class="col-md-7">
-                <div class="card shadow">
-                    <div class="card-header">
-                        
-                        <?php if($this->session->flashdata('status')) { ?>
-                            <div class="alert alert-success">
-                                <?= $this->session->flashdata('status') ?>
-                            </div>
-                        <?php } ?>
+        <div class="row m-0 justify-content-center">
+            <div class="col-10 col-lg-6">
 
-                        <h5>Registration Form</h5>
+                <?php if($this->session->flashdata('registerSuccess')) { ?>
+                    <div class="alert alert-success shadow-sm">
+                        <?= $this->session->flashdata('registerSuccess') ?>
                     </div>
-                    <div class="card-body">
-                        <form action="<?= base_url('register') ?>" method="POST">
+                <?php } ?>
+
+                <?php if($this->session->flashdata('registerFailed')) { ?>
+                    <div class="alert alert-danger shadow-sm">
+                        <?= $this->session->flashdata('registerFailed') ?>
+                    </div>
+                <?php } ?>
+
+                <div class="card shadow-sm">
+                    <form action="<?= base_url('register') ?>" method="POST">
+                        <div class="card-header">
+                            <h5 class="my-2">Registration Form</h5>
+                        </div>
+                        <div class="card-body">
                             <div class="row">
                                 <div class="col-md-6">
                                     <div class="form-group">
                                         <label for="regFirstName">First Name</label>
-                                        <input value="<?= set_value('regFirstName'); ?>" type="text" name="regFirstName" id="regFirstName" class="form-control">
-                                        <small><?= form_error('regFirstName') ?></small>
+                                        <input value="<?= set_value('regFirstName'); ?>" type="text" name="regFirstName" id="regFirstName" class="form-control <?php $formError = ($this->session->flashdata('register')) ? ((form_error('regFirstName')) ? "is-invalid" : (($this->input->post('regFirstName') != "") ? "is-valid" : "")) : ""; echo $formError ?>">
+                                        <small class="invalid-feedback"><?= form_error('regFirstName') ?></small>
                                     </div>
                                 </div>
                                 <div class="col-md-6">
                                     <div class="form-group">
                                         <label for="regLastName">Last Name</label>
-                                        <input value="<?= set_value('regLastName'); ?>" type="text" name="regLastName" id="regLastName" class="form-control">
-                                        <small><?= form_error('regLastName') ?></small>
+                                        <input value="<?= set_value('regLastName'); ?>" type="text" name="regLastName" id="regLastName" class="form-control <?php $formError = ($this->session->flashdata('register')) ? ((form_error('regLastName')) ? "is-invalid" : (($this->input->post('regLastName') != "") ? "is-valid" : "")) : ""; echo $formError ?>">
+                                        <small class="invalid-feedback"><?= form_error('regLastName') ?></small>
                                     </div>
                                 </div>
                                 <div class="col-md-12">
                                     <div class="form-group">
                                         <label for="regEmailAddress">Email Address</label>
-                                        <input value="<?= set_value('regEmailAddress'); ?>" type="text" name="regEmailAddress" id="regEmailAddress" class="form-control">
-                                        <small><?= form_error('regEmailAddress') ?></small>
+                                        <input value="<?= set_value('regEmailAddress'); ?>" type="text" name="regEmailAddress" id="regEmailAddress" class="form-control <?php $formError = ($this->session->flashdata('register')) ? ((form_error('regEmailAddress')) ? "is-invalid" : (($this->input->post('regEmailAddress') != "") ? "is-valid" : "")) : ""; echo $formError ?>">
+                                        <small class="invalid-feedback"><?= form_error('regEmailAddress') ?></small>
                                     </div>
                                 </div>
                                 <div class="col-md-6">
                                     <div class="form-group">
                                         <label for="regPassword">Password</label>
-                                        <input type="password" name="regPassword" id="regPassword" class="form-control">
-                                        <small><?= form_error('regPassword') ?></small>
+                                        <input type="password" name="regPassword" id="regPassword" class="form-control <?php $formError = (form_error('regPassword')) ? "is-invalid" : ""; echo $formError ?>">
+                                        <small class="invalid-feedback"><?= form_error('regPassword') ?></small>
                                     </div>
                                 </div>
                                 <div class="col-md-6">
                                     <div class="form-group">
                                         <label for="regConfirmPassword">Confirm Password</label>
-                                        <input type="password" name="regConfirmPassword" id="regConfirmPassword" class="form-control">
-                                        <small><?= form_error('regConfirmPassword') ?></small>
+                                        <input type="password" name="regConfirmPassword" id="regConfirmPassword" class="form-control <?php $formError = (form_error('regConfirmPassword')) ? "is-invalid" : ""; echo $formError ?>">
+                                        <small class="invalid-feedback"><?= form_error('regConfirmPassword') ?></small>
                                     </div>
                                 </div>
-                                <div class="col-md-12">
-                                    <button type="submit" class="btn btn-primary px-5">Register</button>
-                                </div>
                             </div>
-                        </form>
-                    </div>
+                        </div>
+                        <div class="card-footer">
+                            <div class="form-group m-0 justify-content-end d-flex">
+                                <button type="submit" class="btn btn-primary px-4">Register</button>
+                            </div>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
