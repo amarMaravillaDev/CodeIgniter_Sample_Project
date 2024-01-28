f554a692cdca9ffb34f2a0952737dd84969344f7
Amar Maravilla <amar@clearmindai.com>
2024-01-28 17:55:30 +0800

    Simple CRUD (Adding a new View Component)
    Simple CRUD (Adding a new View Component, the Header View Component)

---
+++ application/views/simple_crud/components/Header.php
@@ -0,0 +1,20 @@
+<?php 
+    // Amar
+    defined('BASEPATH') OR exit('No direct script access allowed'); 
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <title>SIMPLE CRUD</title>
+
+        <!-- Bootstrap CSS -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    
+        <!-- Local CSS -->
+        <link rel="stylesheet" href="../assets/simple_crud/css/style.css">
+    </head>
+    <body>
