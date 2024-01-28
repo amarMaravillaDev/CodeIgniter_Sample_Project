ec87ba150370d2806f36c033346682c1b62576d0
Amar Maravilla <amar@clearmindai.com>
2024-01-28 17:54:21 +0800

    Simple CRUD (Adding a new View)
    Simple CRUD (Adding a new View, the Login View)

---
+++ application/views/simple_crud/authenticate/Login.php
@@ -0,0 +1,12 @@
+<?php 
+    // Amar
+    defined('BASEPATH') OR exit('No direct script access allowed'); 
+?>
+
+<div class="d-flex flex-wrap w-100 h-100">
+    <div class="w-100 d-flex align-items-center position-relative">
+        <div class="w-100 h-100 d-flex flex-column align-items-center justify-content-center bg-primary-subtle">
+            
+        </div>
+    </div>
+</div>
