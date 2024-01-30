59ef914568c71b1411c5b03c9116756be0b85cfa
Amar Maravilla <amar@clearmindai.com>
2024-01-30 19:54:18 +0800

    Simple CRUD (Updating a View Component)
    Simple CRUD (Updating a View Component, the Footer View Component)

--- application/views/simple_crud/components/Footer.php
+++ application/views/simple_crud/components/Footer.php
@@ -5,5 +5,11 @@
 
     <!-- Bootstrap JS -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
+    <!-- JQuery Library -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+    <!-- Local JS -->
+    <script src="../assets/simple_crud/js/Login.js"></script>
 </body>
 </html>
