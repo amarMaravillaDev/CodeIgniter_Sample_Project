1a01b6ade016df1bc2f3337f8fc98976112e25a6
Amar Maravilla <amar@clearmindai.com>
2024-01-30 20:16:54 +0800

    Simple CRUD (Adding a new Script)
    Simple CRUD (Adding a new Script, the Login Script)

---
+++ assets/simple_crud/js/Login.js
@@ -0,0 +1,19 @@
+const login = () => {
+    return {
+        initialize: () => {
+            let loginForm = $('#loginForm');
+            let showPassword = $('#showPassword');
+
+            loginForm.on("submit", (event) => {
+                event.preventDefault();
+            });
+
+            // showPassword.on("click", (event) => {
+            //     event.preventDefault();
+            // });
+        }
+    }
+};
+
+const readyDocument = $document.ready(() => {
+});
