f13196077ed2f63e89a44ffbe876839af5ac5d73
Amar Maravilla <amar@clearmindai.com>
2024-01-30 19:50:44 +0800

    Simple CRUD (Updating a View)
    Simple CRUD (Updating a View, the Login View)

--- application/views/simple_crud/authenticate/Login.php
+++ application/views/simple_crud/authenticate/Login.php
@@ -9,7 +9,7 @@
             <div class="container">
                 <div class="row justify-content-center align-items-center">
                     <div class="col-lg-5 col-md-7 col-sm-9 col">
-                        <form action="">
+                        <form action="#" id="loginForm" >
                             <div class="card shadow-lg border border-0 rounded-4 overflow-hidden">
                                 <div class="card-header d-flex align-items-center gap-3 px-4 py-3">
                                     <i class="fs-3 text-primary fa-solid fa-circle"></i>
@@ -21,32 +21,42 @@
                                 </div>
                                 <div class="card-body d-flex flex-wrap gap-3 p-5">
                                     <div class="d-grid gap-4 align-items-start w-100">
-                                        <div class="input-group">
+                                        <div class="input-group has-validation">
                                             <span class="input-group-text px-4"><i class="fa-solid fa-envelope"></i></span>
                                             <div class="form-floating">
                                                 <input type="text" class="form-control" id="loginEmailAddress" placeholder="Email Address">
                                                 <label for="loginEmailAddress">Email Address</label>
-
-                                                <div class="invalid-feedback">
+                                            </div>
+                                            <div class="invalid-feedback">
                                                     
-                                                </div>
                                             </div>
                                         </div>
-                                        <div class="input-group">
-                                            <span class="input-group-text px-4"><i class="fa-solid fa-lock"></i></span>
+                                        <div class="input-group has-validation">
+                                            <span class="input-group-text px-4">
+                                                <i class="fa-solid fa-lock"></i>
+                                            </span>
                                             <div class="form-floating">
                                                 <input type="password" class="form-control" id="loginPassword" placeholder="Password">
                                                 <label for="loginPassword">Password</label>
-
-                                                <div class="invalid-feedback">
+                                            </div>
+                                            <span id="showPassword" class="input-group-text px-4 d-flex align-items-center justify-content-center">
+                                                <i class="fa-solid fa-eye-slash fs-5"></i>
+                                            </span>
+                                            <div class="invalid-feedback">
                                                     
-                                                </div>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
-                                <div class="card-footer">
+                                <div class="card-footer d-flex align-items-center justify-content-between px-4 py-3">
+                                    <div class="d-flex gap-2">
+                                        No Account Yet?
+                                        <a href="" id="registerLink" class="primary text-decoration-none">Register Here.</a>
+                                    </div>
 
+                                    <button type="submit" class="rounded-5 btn btn-primary focus-ring focus-ring-primary px-5 py-3" name="loginBtn">
+                                        LOG IN
+                                    </button>
                                 </div>
                             </div>
                         </form>
