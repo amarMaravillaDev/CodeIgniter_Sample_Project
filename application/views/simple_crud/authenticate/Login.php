5a78f9501bbd9e401f182d92ab0e1fcc071efce9
Amar Maravilla <amar@clearmindai.com>
2024-01-31 22:50:49 +0800

    Simple CRUD (Updating a View)
    Simple CRUD (Updating a View, the Login View)

--- application/views/simple_crud/authenticate/Login.php
+++ application/views/simple_crud/authenticate/Login.php
@@ -5,24 +5,26 @@
 
 <div class="d-flex flex-wrap w-100 h-100">
     <div class="w-100 d-flex align-items-center position-relative">
-        <div class="w-100 h-100 d-flex flex-column align-items-center justify-content-center gap-4 bg-primary-subtle p-auto">
+        <div class="p-5 w-100 h-100 d-flex flex-column align-items-center justify-content-center gap-4 bg-primary-subtle p-auto">
             <div class="container">
                 <div class="row justify-content-center align-items-center">
-                    <div class="col-lg-5 col-md-7 col-sm-9 col">
-                        <form action="#" id="loginForm" >
-                            <div class="card shadow-lg border border-0 rounded-4 overflow-hidden">
-                                <div class="card-header d-flex align-items-center gap-3 px-4 py-3">
+                    <div class="col-lg-5 col-md-7 col-sm-9">
+                        <form action="#" id="loginForm">
+                            <div class="card shadow-lg border border-0 rounded-5 overflow-hidden">
+                                <div class="card-header d-flex align-items-center gap-3 p-4">
                                     <i class="fs-3 text-primary fa-solid fa-circle"></i>
 
                                     <div class="d-flex gap flex-column text-center">
                                         <p class="fs-4 text-primary m-0 d-flex">USERS FORM</p>
-                                        <p class="m-0 d-flex">(LOG IN AN USER)</p>
+                                        <p class="m-0 d-flex">(LOGIN AN USER)</p>
                                     </div>
                                 </div>
                                 <div class="card-body d-flex flex-wrap gap-3 p-5">
                                     <div class="d-grid gap-4 align-items-start w-100">
                                         <div class="input-group has-validation">
-                                            <span class="input-group-text px-4"><i class="fa-solid fa-envelope"></i></span>
+                                            <span class="input-group-text px-4">
+                                                <i class="fa-solid fa-envelope"></i>
+                                            </span>
                                             <div class="form-floating">
                                                 <input type="text" class="form-control" id="loginEmailAddress" placeholder="Email Address">
                                                 <label for="loginEmailAddress">Email Address</label>
@@ -48,10 +50,10 @@
                                         </div>
                                     </div>
                                 </div>
-                                <div class="card-footer d-flex align-items-center justify-content-between px-4 py-3">
-                                    <div class="d-flex gap-2">
+                                <div class="card-footer d-flex align-items-center flex-wrap justify-content-between gap-4 p-4 ps-md-5">
+                                    <div class="d-flex gap-2 justify-content-center justify-content-md-start">
                                         No Account Yet?
-                                        <a href="" id="registerLink" class="primary text-decoration-none">Register Here.</a>
+                                        <a href="<?= base_url('simple_crud/register') ?>" id="registerLink" class="primary text-decoration-none">Register Here.</a>
                                     </div>
 
                                     <button type="submit" class="rounded-5 btn btn-primary focus-ring focus-ring-primary px-5 py-3" name="loginBtn">
