c47fc78a3834f6b4223c1d9e22b20fb2123a5abb
Amar Maravilla <amar@clearmindai.com>
2024-01-31 21:57:11 +0800

    Simple CRUD (Adding new Routes)
    Simple CRUD (Adding new Routes for Register Controller in routes Config)

--- application/config/routes.php
+++ application/config/routes.php
@@ -45,3 +45,7 @@
     // Login Controller Routes
     $route['simple_crud/login']['GET'] = 'simple_crud/authenticate/Login';
     $route['simple_crud/login']['POST'] = 'simple_crud/authenticate/Login/login';
+
+    // Register Controller Routes
+    $route['simple_crud/register']['GET'] = 'simple_crud/authenticate/Register';
+    $route['simple_crud/register']['POST'] = 'simple_crud/authenticate/Register/register';
