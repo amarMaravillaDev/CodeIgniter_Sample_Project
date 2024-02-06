c56968cc2a45414310305594b8f37a43749f707a
Amar Maravilla <amar@clearmindai.com>
2024-02-06 11:46:45 +0800

    Simple CRUD (Updating a JS)
    Simple CRUD (Updating a JS, the Login JS)

--- assets/simple_crud/js/Login.js
+++ assets/simple_crud/js/Login.js
@@ -1,19 +1,19 @@
-const login = () => {
-    return {
-        initialize: () => {
-            let loginForm = $('#loginForm');
-            let showPassword = $('#showPassword');
+// const login = () => {
+//     return {
+//         initialize: () => {
+//             let loginForm = $('#loginForm');
+//             let showPassword = $('#showPassword');
 
-            loginForm.on("submit", (event) => {
-                event.preventDefault();
-            });
+//             loginForm.on("submit", (event) => {
+//                 event.preventDefault();
+//             });
 
-            // showPassword.on("click", (event) => {
-            //     event.preventDefault();
-            // });
-        }
-    }
-};
+//             // showPassword.on("click", (event) => {
+//             //     event.preventDefault();
+//             // });
+//         }
+//     }
+// };
 
-const readyDocument = $('document').ready(() => {
-});
+// const readyDocument = $('document').ready(() => {
+// });
