a670ad029555e1c4b5b095423a400254a06346f8
Amar Maravilla <amar@clearmindai.com>
2024-02-06 23:20:51 +0800

    Simple CRUD (Adding a new View Component)
    Simple CRUD (Adding a new View Component, the NavBar View Component)

---
+++ application/views/simple_crud/components/NavBar.php
@@ -0,0 +1,19 @@
+<?php 
+    // Amar
+    defined('BASEPATH') OR exit('No direct script access allowed'); 
+?>
+
+<nav class="navbar navbar-expand-lg bg-primary bg-gradient p-0">
+    <div class="px-5 py-2 d-flex justify-content-between align-items-center w-100">
+        <div class="d-flex align-items-center gap-3 p-4 px-5">
+            <i class="fs-3 text-light fa-solid fa-circle"></i>
+
+            <div class="d-flex gap flex-column text-center">
+                <p class="fs-4 text-light m-0 d-flex">SIMPLE CRUD</p>
+            </div>
+        </div>
+        <div class="d-flex align-items-center gap-3 p-4 px-5">
+            popo
+        </div>
+    </div>
+</nav>
