41be587183216780f409dcae50daf4bb949f18b7
Amar Maravilla <amar@clearmindai.com>
2024-02-06 23:22:02 +0800

    Simple CRUD (Adding a new View Component)
    Simple CRUD (Adding a new View Component, the SideBar View Component)

---
+++ application/views/simple_crud/components/SideBar.php
@@ -0,0 +1,10 @@
+<?php 
+    // Amar
+    defined('BASEPATH') OR exit('No direct script access allowed'); 
+?>
+
+<aside class="w-25 d-flex border-end border-primary">
+    <div>
+        popo
+    </div>
+</aside>
