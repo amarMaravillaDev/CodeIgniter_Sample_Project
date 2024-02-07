7c8f852d6b7aa8003b3bcaa6265c5b621e5c9bfe
Amar Maravilla <amar@clearmindai.com>
2024-02-08 00:01:48 +0800

    Simple CRUD (Updating a View Component)
    Simple CRUD (Updating a View Component, the NavBar View Component)

--- application/views/simple_crud/components/NavBar.php
+++ application/views/simple_crud/components/NavBar.php
@@ -13,7 +13,27 @@
             </div>
         </div>
         <div class="d-flex align-items-center gap-3 p-4 px-5">
-            popo
+            <div class="text-light">
+                Hello, <?= $this->session->userdata('usersDetails')['FIRST_NAME']; ?>
+            </div>
+            <div class="position-relative shadow bg-light borderRadiusFull d-flex align-items-center justify-content-center p-4">
+                <i class="fs-5 fa-solid fa-user text-primary d-flex align-items-center justify-content-center"></i>
+
+                <button type="button" data-bs-toggle="dropdown" aria-expanded="false" class="border-0 bg-primary shadow position-absolute end-0 bottom-0 p-1 borderRadiusFull">
+                    <div class="d-flex align-items-center justify-content-center squareXSm">
+                        <i class="fa-solid fa-caret-down d-flex align-items-center justify-content-center text-light"></i>
+                    </div>
+                </button>
+
+                <ul class="dropdown-menu dropDownLogout">
+                    <li class="">
+                        <a class="dropdown-item d-flex align-items-center justify-content-center gap-3" href="<?= base_url('simple_crud/logout') ?>">
+                            <i class="fa-solid fa-arrow-right-from-bracket"></i>
+                            Log Out
+                        </a>
+                    </li>
+                </ul>
+            </div>
         </div>
     </div>
 </nav>
