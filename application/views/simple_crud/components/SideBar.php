34d51430cc48f6cd6492be69a9563c804716803d
Amar Maravilla <amar@clearmindai.com>
2024-02-09 23:31:14 +0800

    Simple CRUD (Updating a View Component)
    Simple CRUD (Updating a View Component, the SideBar View Component)

--- application/views/simple_crud/components/SideBar.php
+++ application/views/simple_crud/components/SideBar.php
@@ -3,7 +3,7 @@
     defined('BASEPATH') OR exit('No direct script access allowed'); 
 ?>
 
-<aside class="w-25 d-flex border-end border-primary">
+<aside class="w-100 d-flex border-end border-primary sideBar overflow-auto">
     <div>
         popo
     </div>
