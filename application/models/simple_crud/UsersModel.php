ca06bb7f08bf8cd49e20ae5f69ae7ddc3d88e9f8
Amar Maravilla <amar@clearmindai.com>
2024-02-12 17:36:20 +0800

    Simple CRUD (Updating a Model)
    Simple CRUD (Updating a Model, the UsersModel Model)

--- application/models/simple_crud/UsersModel.php
+++ application/models/simple_crud/UsersModel.php
@@ -23,9 +23,47 @@
             }
         }
 
-        public function usersList() {
-            $query = $this->db->get('SIMPLE_CRUD_USERS_SEEDS');
+        // public function usersListSeeds() {
+        //     $query = $this->db->get('SIMPLE_CRUD_USERS_SEEDS');
 
-            return $query->result_array();
+        //     return $query->result_array();
+        // }
+
+        // public function usersList() {
+        //     $query = $this->db->get('SIMPLE_CRUD_USERS');
+
+        //     return $query->result_array();
+        // }
+
+        public function searchUsers($pageFilter = "", $startIndex = "", $searchFor = "", $rowCount = 0) {
+            // echo '<script> console.log(`Page Filter (Model): `, ' . json_encode($pageFilter) . '); </script>';
+            // echo '<script> console.log(`Start Index (Model): `, ' . json_encode($startIndex) . '); </script>';
+
+            if($pageFilter != "" || $startIndex != "") {
+                $this->db->limit($pageFilter, $startIndex);
+            }
+
+            if($searchFor) {
+                $this->db->like('FIRST_NAME', $searchFor, 'both');
+                $this->db->or_like('MIDDLE_NAME', $searchFor, 'both');
+                $this->db->or_like('LAST_NAME', $searchFor, 'both');
+                $this->db->or_like('SUFFIX', $searchFor, 'both');
+                $this->db->or_like('GENDER', $searchFor, 'both');
+                $this->db->or_like('BIRTH_DATE', $searchFor, 'both');
+                $this->db->or_like('AGE', $searchFor, 'both');
+                $this->db->or_like('CONTACT_NUMBER', $searchFor, 'both');
+                $this->db->or_like('EMAIL_ADDRESS', $searchFor, 'both');
+            }
+
+            if($rowCount == 1) {
+                $query = $this->db->get('SIMPLE_CRUD_USERS_SEEDS');
+
+                return $query->num_rows();
+            }
+            else {
+                $query = $this->db->get('SIMPLE_CRUD_USERS_SEEDS');
+
+                return $query->result_array();
+            }
         }
     }
