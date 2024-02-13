2b40127a16dbe05a48d9062eedac8d76f2420147
Amar Maravilla <amar@clearmindai.com>
2024-02-13 18:35:19 +0800

    Simple CRUD (Adding a new JS)
    Simple CRUD (Adding a new JS, the Users JS)

---
+++ assets/simple_crud/js/Users.js
@@ -0,0 +1,18 @@
+const usersFunctions = (() => {
+    return {
+        initialize: () => {
+            // Dropdowns
+            $(`.usersFilter`).on("change", function() {
+                // Console Log
+                console.log(`Users Filter: ${$(this).val()}`);
+
+                // Function
+                $(`#usersSearchForm`).submit();
+            });
+        }
+    }
+})();
+
+$(document).ready(() => {
+    usersFunctions.initialize();
+});
