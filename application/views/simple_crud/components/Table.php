923835c9ed2fff06a113f876359d7b272e39147d
Amar Maravilla <amar@clearmindai.com>
2024-02-13 18:34:25 +0800

    Simple CRUD (Updating a View Component)
    Simple CRUD (Updating a View Component, the Table View Component)

--- application/views/simple_crud/components/Table.php
+++ application/views/simple_crud/components/Table.php
@@ -1,38 +1,46 @@
-<?php 
-    defined('BASEPATH') OR exit('No direct script access allowed'); 
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
 ?>
 
-<div class="card overflow-hidden d-flex flex-column w-100 h-100 border border-primary">
-    <div class="overflow-auto w-100 h-100 d-flex">
-        <table class="w-100 h-100 overflow-auto table table-hover table-primary table-striped border-primary m-0 p-0 w-100 h-100 overflow-auto">
+    <div class="overflow-auto w-100 h-100 table-responsive-sm">
+        <table class="table table-hover table-striped border-primary m-0 p-0">
             <thead>
-                <th class="bg-primary text-light">FIRST NAME</th>
-                <th class="bg-primary text-light">MIDDLE NAME</th>
-                <th class="bg-primary text-light">LAST NAME</th>
-                <th class="bg-primary text-light">SUFFIX</th>
-                <th class="bg-primary text-light">GENDER</th>
-                <th class="bg-primary text-light">BIRTH DATE</th>
-                <th class="bg-primary text-light">AGE</th>
-                <th class="bg-primary text-light">CONTACT NUMBER</th>
-                <th class="bg-primary text-light">EMAIL ADDRESS</th>
-            </thead>
-            <tbody class="table-group-divider border-light">
 
-                <?php foreach ($usersList as $user) { ?>
+                <?php foreach ($table['tableColumns'] as $columnName) { ?>
+                    <th class="bg-primary text-light position-sticky top-0 text-nowrap p-0">
+                        <a href="<?= base_url('simple_crud/users?sortBy=' . $columnName["dbColumn"] . '&orderBy=' . ($sortBy == $columnName["dbColumn"] && $orderBy == 'ASC' ? 'DESC' : 'ASC') . ($rowFilter ? '&rowFilter=' . $rowFilter : "") . ($searchFor ? '&searchFor=' . $searchFor : "") . ($page ? '&page=' . $page : "")); ?>" 
+                            class="text-light d-flex align-items-center justify-content-between gap-4 p-4 btn btn-primary border-0 rounded-0">
+                            <?= $columnName["tableColumn"] ?>
+
+                            <div class="d-flex flex-column gap-1">
+                                <i class="fa-solid fa-sort-up d-flex align-items-center justify-content-center <?= $sortBy == $columnName["dbColumn"] ? ($orderBy == "ASC" ? "text-primary-emphasis" : "") : "" ?>"></i>
+                                <i class="fa-solid fa-sort-down d-flex align-items-center justify-content-center <?= $sortBy == $columnName["dbColumn"] ? ($orderBy == "DESC" ? "text-primary-emphasis" : "") : "" ?>"></i>
+                            </div>
+                        </a>
+                    </th>
+                <?php } ?>
+
+            </thead>
+            <tbody class="border-secondary-subtle">
+
+                <?php foreach ($table['tableData'] as $row) { ?>
                     <tr>
-                        <td><?= $user['FIRST_NAME'] ?></td>
-                        <td><?= $user['MIDDLE_NAME'] ?></td>
-                        <td><?= $user['LAST_NAME'] ?></td>
-                        <td><?= $user['SUFFIX'] ?></td>
-                        <td><?= $user['GENDER'] ?></td>
-                        <td><?= $user['BIRTH_DATE'] ?></td>
-                        <td><?= $user['AGE'] ?></td>
-                        <td><?= $user['CONTACT_NUMBER'] ?></td>
-                        <td><?= $user['EMAIL_ADDRESS'] ?></td>
+
+                        <?php
+                        foreach ($row as $columnName => $columnValue) {
+                            if (!in_array($columnName, $table['tableColumnsNotShown'])) {
+                                ?>
+                                <td class="text-nowrap p-4">
+                                    <?= $columnValue ?>
+                                </td>
+                            <?php
+                            }
+                        }
+                        ?>
+
                     </tr>
                 <?php } ?>
 
             </tbody>
         </table>
     </div>
-</div>
