9e6c86e5322aa72209965a2a793d95ccdc067598
Amar Maravilla <amar@clearmindai.com>
2024-02-13 18:33:46 +0800

    Simple CRUD (Updating a View Component)
    Simple CRUD (Updating a View Component, the NavBar View Component)

--- application/views/simple_crud/components/NavBar.php
+++ application/views/simple_crud/components/NavBar.php
@@ -4,20 +4,20 @@
 ?>
 
 <nav class="navbar navbar-expand-lg bg-primary bg-gradient p-0">
-    <div class="px-5 py-2 d-flex justify-content-between align-items-center w-100">
-        <div class="d-flex align-items-center gap-3 p-4 px-5">
+    <div class="px-5 py-4 d-flex justify-content-between align-items-center w-100">
+        <div class="d-flex align-items-center gap-3">
             <i class="fs-3 text-light fa-solid fa-circle"></i>
 
             <div class="d-flex gap flex-column text-center">
                 <p class="fs-4 text-light m-0 d-flex">SIMPLE CRUD</p>
             </div>
         </div>
-        <div class="d-flex align-items-center gap-3 p-4 px-5">
+        <div class="d-flex align-items-center gap-3">
             <div class="text-light">
                 Hello, <?= $this->session->userdata('usersDetails')['FIRST_NAME']; ?>
             </div>
             <div class="position-relative shadow bg-light borderRadiusFull d-flex align-items-center justify-content-center p-4">
-                <i class="fs-5 fa-solid fa-user text-primary d-flex align-items-center justify-content-center"></i>
+                <i class="fs-5 fa-solid fa-user text-primary"></i>
 
                 <button type="button" data-bs-toggle="dropdown" aria-expanded="false" class="focus-ring focus-ring-primary border-1 border border-light bg-primary position-absolute end-0 bottom-0 p-1 borderRadiusFull">
                     <div class="d-flex align-items-center justify-content-center squareXSm">
