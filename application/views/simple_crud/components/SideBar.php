04e10c3fc99bb6a268979605a73570096e083f97
Amar Maravilla <amar@clearmindai.com>
2024-02-18 20:30:17 +0800

    Simple CRUD (Updating a View Component)
    Simple CRUD (Updating a View Component, the SideBar View Component)

--- application/views/simple_crud/components/SideBar.php
+++ application/views/simple_crud/components/SideBar.php
@@ -5,14 +5,25 @@
 
 <aside class="w-100 h-100 d-flex border-end border-primary sideBar overflow-y-auto">
     <nav class="navbar w-100 h-100 p-0">
-        <ul class="navbar-nav w-100 h-100 d-flex flex-column gap-4 p-4">
-            <li class="nav-item fs-5">
-                <a class="nav-link p-4 d-flex align-items-center gap-3 btn btn-primary text-primary " href="#">
-                    <i class="fa-solid fa-shapes fs-5"></i>
+        <ul class="navbar-nav w-100 h-100 d-flex flex-column p-1 gap-1">
+            <li class="nav-item">
+                <a href="<?= base_url('simple_crud/users') ?>" class="focus-ring focus-ring-primary border-0 p-4 d-flex align-items-center gap-4 btn btn-outline-primary">
+                    <i class="fa-solid fa-shapes"></i>
                     Dashboard
                 </a>
             </li>
-            
+            <li class="nav-item">
+                <a href="#" class="focus-ring focus-ring-primary border-0 p-4 d-flex align-items-center gap-4 btn btn-outline-primary">
+                    <i class="fa-solid fa-user"></i>
+                    My Profile
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="focus-ring focus-ring-primary border-0 p-4 d-flex align-items-center gap-4 btn btn-outline-primary">
+                    <i class="fa-solid fa-gear"></i>
+                    Settings
+                </a>
+            </li>
         </ul>
     </nav>
 </aside>
