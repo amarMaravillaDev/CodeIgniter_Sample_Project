c41a34d83961f6b0d3878213e573832a74d12c63
Amar Maravilla <amar@clearmindai.com>
2024-02-22 23:03:38 +0800

    Simple CRUD (Updating a View Component)
    Simple CRUD (Updating a View Component, the Footer View Component)

--- application/views/simple_crud/components/Footer.php
+++ application/views/simple_crud/components/Footer.php
@@ -1,19 +1,24 @@
-<?php 
-    // Amar
-    defined('BASEPATH') OR exit('No direct script access allowed'); 
+<?php
+// Amar
+defined('BASEPATH') or exit('No direct script access allowed');
 ?>
 
-    <!-- Bootstrap JS -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+<!-- Bootstrap JS -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+	integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+	crossorigin="anonymous"></script>
 
-    <!-- JQuery Library -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<!-- JQuery Library -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"
+	integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g=="
+	crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
-    <!-- Local JS -->
-    <script src="../assets/simple_crud/js/Global.js"></script>
+<!-- Local JS -->
+<script src="/assets/simple_crud/js/Global.js"></script>
 
-    <?php if($document['script']) { ?>
-        <script src="../assets/simple_crud/js/<?= $document['script'] ?>.js"></script>
-    <?php } ?>
+<?php if ($documentData['script']) { ?>
+	<script src="/assets/simple_crud/js/<?= $documentData['script'] ?>.js"></script>
+<?php } ?>
 </body>
+
 </html>
