737facb1a9bce4d2f435481a326bd2bd812ca604
Amar Maravilla <amar@clearmindai.com>
2024-02-22 23:06:16 +0800

    Simple CRUD (Updating a JS)
    Simple CRUD (Updating a JS, the Global JS)

--- assets/simple_crud/js/Global.js
+++ assets/simple_crud/js/Global.js
@@ -1,12 +1,70 @@
 const globalFunctions = (() => {
-    return {
-        initialize: () => {
-            // Loaders
-            $(`.tableLoader`).fadeOut(`slow`);
-        }
-    }
+	return {
+		initialize: () => {
+			// Toast
+			const toasts = $(`#liveToast`);
+
+			if (toasts.length) {
+				const toastBS = bootstrap.Toast.getOrCreateInstance(toasts);
+
+				toastBS.show();
+			}
+
+			// Show Password
+			$(`.showPassword`).on(`click`, function (event) {
+				event.preventDefault();
+
+				let showPasswordIcon = $(this).find(`span`);
+				let passwordInput = $(this)
+					.parent()
+					.find(`.password`)
+					.find(`.password`);
+
+				if ($.trim(showPasswordIcon.text()) == `visibility`) {
+					console.log(showPasswordIcon.text() + " 1");
+					showPasswordIcon.text(`visibility_off`);
+
+					passwordInput.attr(`type`, `text`);
+				} else {
+					console.log(showPasswordIcon.text() + " 2");
+					showPasswordIcon.text(`visibility`);
+
+					passwordInput.attr(`type`, `password`);
+				}
+			});
+
+			// Navigation Links
+			let currentURL = window.location.href;
+
+			$(`.sideBar a`).each(function () {
+				if ($(this).attr(`href`) === currentURL.split(`?`)[0]) {
+					$(this).addClass(`active`);
+				}
+			});
+
+			// Loaders
+			$(`.tableLoader`).fadeOut(`slow`);
+
+			// Table Headers
+			$(`table thead th a`).on(`click`, function () {
+				$(`.tableLoader`).show();
+			});
+
+			// Table Form
+			$(`.tableForm`).on(`submit`, function () {
+				$(`.tableLoader`).show();
+			});
+
+			// Paginations Links
+			$(`.paginationLinks .page-item a`).on(`click`, function () {
+				if ($(this).attr(`href`) != `#`) {
+					$(`.tableLoader`).show();
+				}
+			});
+		},
+	};
 })();
 
 $(document).ready(() => {
-    globalFunctions.initialize();
+	globalFunctions.initialize();
 });
