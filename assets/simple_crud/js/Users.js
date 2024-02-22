9e98eee9ac7a89729f82040607b73e5afb94ea5f
Amar Maravilla <amar@clearmindai.com>
2024-02-22 23:06:49 +0800

    Simple CRUD (Updating a JS)
    Simple CRUD (Updating a JS, the Users JS)

--- assets/simple_crud/js/Users.js
+++ assets/simple_crud/js/Users.js
@@ -1,18 +1,18 @@
 const usersFunctions = (() => {
-    return {
-        initialize: () => {
-            // Dropdowns
-            $(`.usersFilter`).on("change", function() {
-                // Console Log
-                console.log(`Users Filter: ${$(this).val()}`);
+	return {
+		initialize: () => {
+			// Dropdowns
+			$(`.usersFilter`).on("change", function () {
+				// Console Log
+				console.log(`Users Filter: ${$(this).val()}`);
 
-                // Function
-                $(`#usersSearchForm`).submit();
-            });
-        }
-    }
+				// Function
+				$(`#usersSearchForm`).submit();
+			});
+		},
+	};
 })();
 
 $(document).ready(() => {
-    usersFunctions.initialize();
+	usersFunctions.initialize();
 });
