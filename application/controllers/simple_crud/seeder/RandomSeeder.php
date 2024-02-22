ecafcc3ec110a8d2efa137c545bf761a331b53c2
Amar Maravilla <amar@clearmindai.com>
2024-02-22 22:58:45 +0800

    Simple CRUD (Updating a Controller)
    Simple CRUD (Updating a Controller, the RandomSeeder Controller)

--- application/controllers/simple_crud/seeder/RandomSeeder.php
+++ application/controllers/simple_crud/seeder/RandomSeeder.php
@@ -1,22 +1,26 @@
-<?php 
-    defined('BASEPATH') OR exit('No direct script access allowed');
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
 
-    class RandomSeeder extends CI_Controller {
-        public function __construct() {
-            parent::__construct();
+class RandomSeeder extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
 
-            // Model
-            $this->load->model('simple_crud/SeederModel', 'Seeder');
-        }
-
-        public function index() {
-
-        }
-
-        public function seed() {
-            $tableName = 'SIMPLE_CRUD_USERS_SEEDS'; 
-            $recordLimit = 50;
-
-            $this->Seeder->seedData($tableName, $recordLimit);
-        }
+        // Model
+        $this->load->model('simple_crud/SeederModel', 'Seeder');
     }
+
+    public function index()
+    {
+
+    }
+
+    public function seed()
+    {
+        $tableName = 'SIMPLE_CRUD_USERS_SEEDS';
+        $recordLimit = 50;
+
+        $this->Seeder->seedData($tableName, $recordLimit);
+    }
+}
