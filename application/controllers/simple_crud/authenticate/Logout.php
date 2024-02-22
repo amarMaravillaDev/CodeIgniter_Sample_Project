9883269cef52552a60326e1f4308f23e442d84a6
Amar Maravilla <amar@clearmindai.com>
2024-02-22 22:57:59 +0800

    Simple CRUD (Updating a Controller)
    Simple CRUD (Updating a Controller, the Logout Controller)

--- application/controllers/simple_crud/authenticate/Logout.php
+++ application/controllers/simple_crud/authenticate/Logout.php
@@ -1,23 +1,34 @@
-<?php 
-    defined('BASEPATH') OR exit('No direct script access allowed');
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
 
-    class Logout extends CI_Controller {
-        public function __construct() {
-            parent::__construct();
+class Logout extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
 
-            $this->load->model('simple_crud/AuthenticationModel', 'Authenticate');
-        }
-
-        public function index() {
-
-        }
-
-        public function logout() {
-            $this->session->unset_userdata('userType');
-            $this->session->unset_userdata('usersDetails');
-
-            $this->session->set_flashdata('loginToast', array('toastStatus' => 'success', 'toastMessage' => 'You are Logged Out Successfully.'));
-
-            redirect(base_url('simple_crud/login'));
-        }
+        $this->load->model('simple_crud/AuthenticationModel', 'Authenticate');
     }
+
+    public function index()
+    {
+
+    }
+
+    public function logout()
+    {
+        $this->session->unset_userdata('userType');
+        $this->session->unset_userdata('usersDetails');
+
+        $this->session->set_flashdata(
+            'loginToast',
+            array(
+                'toastStatus' => 'success',
+                'toastMessage' => 'You are Logged Out Successfully.',
+                'toastIcon' => 'check_circle'
+            )
+        );
+
+        redirect(base_url('simple_crud/login'));
+    }
+}
