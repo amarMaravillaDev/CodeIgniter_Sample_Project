22fc918d6ced95bbf097196f4b58deb941c74a1e
Amar Maravilla <amar@clearmindai.com>
2024-02-22 22:59:40 +0800

    Simple CRUD (Updating a Model)
    Simple CRUD (Updating a Model, the AuthenticationModel Model)

--- application/models/simple_crud/AuthenticationModel.php
+++ application/models/simple_crud/AuthenticationModel.php
@@ -1,26 +1,28 @@
 <?php
-    defined('BASEPATH') or exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-    class AuthenticationModel extends CI_Model {
-        public function __construct() {
-            parent::__construct();
+class AuthenticationModel extends CI_Model
+{
+    public function __construct()
+    {
+        parent::__construct();
 
-            if(!$this->session->has_userdata('userType')) {
-                $this->session->set_flashdata('loginToast', array('toastStatus' => 'warning', 'toastMessage' => 'Log In First.'));
+        if (!$this->session->has_userdata('userType')) {
+            $this->session->set_flashdata('loginToast', array('toastStatus' => 'warning', 'toastMessage' => 'Log In First.'));
 
-                redirect(base_url('simple_crud/login'));
-            }
-        }
-
-        public function users() {
-            if($this->session->has_userdata('userType')) {
-                if($this->session->userdata('userType') == '1') {
-                }
-            } 
-            else {
-                $this->session->set_flashdata('loginToast', array('toastStatus' => 'warning', 'toastMessage' => 'Log In First.'));
-
-                redirect(base_url('simple_crud/login'));
-            }
+            redirect(base_url('simple_crud/login'));
         }
     }
+
+    public function users()
+    {
+        if ($this->session->has_userdata('userType')) {
+            if ($this->session->userdata('userType') == '1') {
+            }
+        } else {
+            $this->session->set_flashdata('loginToast', array('toastStatus' => 'warning', 'toastMessage' => 'Log In First.'));
+
+            redirect(base_url('simple_crud/login'));
+        }
+    }
+}
