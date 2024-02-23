ed4792ce74b46b5e09a3b0a7a4d9fae54fa6acfd
Amar Maravilla <amar@clearmindai.com>
2024-02-23 23:10:08 +0800

    Simple CRUD (Updating a View Component)
    Simple CRUD (Updating a View Component, the DashBoard View Component)

--- application/views/simple_crud/users/DashBoard.php
+++ application/views/simple_crud/users/DashBoard.php
@@ -3,8 +3,32 @@ defined('BASEPATH') or exit('No direct script access allowed');
 ?>
 
 <main class="border-end border-primary w-100 h-100 d-flex flex-column align-items-center overflow-auto">
+    <!-- Top Navigations -->
+    <div class="col-12 p-4 border-bottom border-primary">
+        <div class="row gap-3 gap-lg-0">
+            <div class="col-lg-4 fs-5 d-flex align-items-center gap-3 text-primary ">
+                <!-- <i class="fs-4 fa-solid fa-circle"></i> -->
+                <span class="material-symbols-rounded">
+                    space_dashboard
+                </span>
+
+                DASHBOARD
+            </div>
+        </div>
+    </div>
+
     <!-- Main Content -->
-    <div class="p-0 w-100 h-100 overflow-auto position-relative">
-        
-    </div>=
+    <div class="p-0 w-100 h-100 overflow-auto position-relative p-4 bg-light">
+        <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 row-gap-4">
+
+            <?php
+            if (count($viewsData['cards'])) {
+                foreach ($viewsData['cards'] as $card) {
+                    $this->load->view('simple_crud/components/Cards', array("cards" => $card));
+                }
+            }
+            ?>
+
+        </div>
+    </div>
 </main>
