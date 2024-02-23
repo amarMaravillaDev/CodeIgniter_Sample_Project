5b853c48b47c1b1ffd136cad7abdaacd0b24a286
Amar Maravilla <amar@clearmindai.com>
2024-02-23 23:09:48 +0800

    Simple CRUD (Updating a View Component)
    Simple CRUD (Updating a View Component, the SideBar View Component)

--- application/views/simple_crud/components/SideBar.php
+++ application/views/simple_crud/components/SideBar.php
@@ -3,7 +3,7 @@
 defined('BASEPATH') or exit('No direct script access allowed');
 ?>
 
-<aside class="w-100 h-100 d-flex border-start border-end border-primary sideBar">
+<aside class="w-100 h-100 d-flex border-start border-end border-primary bg-light sideBar">
     <nav class="navbar w-100 h-100 p-0">
         <ul class="navbar-nav w-100 h-100 d-flex flex-column p-4 gap-3 overflow-y-auto">
             <li class="nav-item">
