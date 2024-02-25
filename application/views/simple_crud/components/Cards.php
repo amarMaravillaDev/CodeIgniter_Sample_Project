db7b70cad4a52e29c2a38c469fc5a52d0ed1440a
Amar Maravilla <amar@clearmindai.com>
2024-02-25 23:52:58 +0800

    Simple CRUD (Updating a View Component)
    Simple CRUD (Updating a View Component, the Cards View Component)

--- application/views/simple_crud/components/Cards.php
+++ application/views/simple_crud/components/Cards.php
@@ -2,17 +2,21 @@
 defined('BASEPATH') or exit('No direct script access allowed');
 ?>
 
-<div class="col">
-    <div class="fadeIn card border-0 shadow-sm rounded-4 p-4">
-        <div class="card-body gap-3 flex-column flex-lg-row row justify-content-center align-items-center <?= $cards['cardTextColor'] ?>">
-            <div class="col d-flex align-items-center justify-content-center">
+<div class="col fadeIn">
+    <div class="d-flex align-items-center justify-content-center dashBoardCard card border-0 shadow-sm rounded-4 py-5">
+        <div class="card-body p-0 gap-3 gap-lg-0 w-100 flex-column flex-lg-row row justify-content-center align-items-center <?= $cards['cardTextColor'] ?>">
+            <div class="col-12 col-lg-6 d-flex align-items-center justify-content-center">
                 <span class="material-symbols-rounded tableDataIcon">
                     <?= $cards['cardIcon'] ?>
                 </span>
             </div>
-            <div class="col d-flex flex-column align-items-center align-items-lg-start">
-                <div class="fw-bold fs-1"><?= $cards['cardTotalNumber'] ?></div>
-                <?= $cards['cardTitle'] ?>
+            <div class="col-12 col-lg-6 row align-items-center align-items-lg-start">
+                <div class="col-12 fw-bold fs-1 d-flex justify-content-center justify-content-lg-start">
+                    <?= $cards['cardTotalNumber'] ?>
+                </div>
+                <div class="col-12 d-flex justify-content-center justify-content-lg-start">
+                    <?= $cards['cardTitle'] ?>
+                </div>
             </div>
         </div>
     </div>
