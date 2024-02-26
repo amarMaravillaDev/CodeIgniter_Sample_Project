b8fe20798f537ce9b9e9770420a56e29016f8c92
Amar Maravilla <amar@clearmindai.com>
2024-02-26 15:17:29 +0800

    Simple CRUD (Updating a View)
    Simple CRUD (Updating a View, the MyProfile View)

--- application/views/simple_crud/users/MyProfile.php
+++ application/views/simple_crud/users/MyProfile.php
@@ -2,6 +2,27 @@
 defined('BASEPATH') or exit('No direct script access allowed');
 ?>
 
-<div class="col">
-    
-</div>
+<main class="border-end border-primary w-100 h-100 d-flex flex-column align-items-center overflow-auto">
+    <!-- Top Navigations -->
+    <div class="col-12 p-4 border-bottom border-primary">
+        <div class="row gap-3 gap-lg-0">
+            <div class="col-lg-4 fs-5 d-flex align-items-center gap-3 text-primary ">
+                <!-- <i class="fs-4 fa-solid fa-circle"></i> -->
+                <span class="material-symbols-rounded">
+                    user_attributes
+                </span>
+
+                <h5>MY PROFILE</h5>
+            </div>
+        </div>
+    </div>
+
+    <!-- Main Content -->
+    <div class="p-0 w-100 h-100 overflow-auto position-relative p-4 bg-light">
+        <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 row-gap-4">
+
+            
+
+        </div>
+    </div>
+</main>
