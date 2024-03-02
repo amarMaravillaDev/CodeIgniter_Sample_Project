be331bc798763faa1a4cea3ded0d923b14915819
Amar Maravilla <amar@clearmindai.com>
2024-03-02 23:00:43 +0800

    Simple CRUD (Updating a View)
    Simple CRUD (Updating a View, the DashBoard View)

--- application/views/simple_crud/users/DashBoard.php
+++ application/views/simple_crud/users/DashBoard.php
@@ -2,7 +2,7 @@
 defined('BASEPATH') or exit('No direct script access allowed');
 ?>
 
-<main class="border-end border-primary w-100 h-100 d-flex flex-column align-items-center overflow-auto">
+<main class="bg-light border-end border-primary w-100 h-100 d-flex flex-column align-items-center overflow-auto">
     <!-- Top Navigations -->
     <div class="col-12 p-4 border-bottom border-primary">
         <div class="row">
@@ -18,7 +18,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
     </div>
 
     <!-- Main Content -->
-    <div class="w-100 h-100 overflow-auto position-relative p-4 bg-light">
+    <div class="w-100 h-100 overflow-auto position-relative p-4">
         <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 row-gap-4">
 
             <?php
