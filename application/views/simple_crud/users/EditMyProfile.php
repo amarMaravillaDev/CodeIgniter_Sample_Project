775a7aacba5d5f83285025bff2950f4afeece9fd
Amar Maravilla <amar@clearmindai.com>
2024-03-03 20:18:35 +0800

    Simple CRUD (Updating a View)
    Simple CRUD (Updating a View, the EditMyProfile View)

--- application/views/simple_crud/users/EditMyProfile.php
+++ application/views/simple_crud/users/EditMyProfile.php
@@ -2,7 +2,7 @@
 defined('BASEPATH') or exit('No direct script access allowed');
 ?>
 
-<main class="border-end border-primary w-100 h-100 d-flex flex-column align-items-center">
+<main class="bg-light border-end border-primary w-100 h-100 d-flex flex-column align-items-center">
     <!-- Top Navigations -->
     <div class="col-12 p-4 border-bottom border-primary">
         <div class="row">
@@ -18,95 +18,70 @@ defined('BASEPATH') or exit('No direct script access allowed');
     </div>
 
     <!-- Main Content -->
-    <div class="w-100 h-100 overflow-auto position-relative bg-light d-flex flex-column">
-        <div class="d-flex align-items-center justify-content-center">
-            <div class="w-100 position-relative profileBoard d-flex justify-content-center justify-content-md-start">
-                <div class="w-100 h-100">
-                    <?php if ($usersDetails['coverPhoto']) { ?>
-                        <img src="" alt="coverPhoto"
-                            class="d-flex align-items-center justify-content-center bg-primary bg-opacity-25 w-100 h-100">
-                    <?php } else { ?>
-                        <div class="d-flex align-items-center justify-content-center bg-primary bg-opacity-25 w-100 h-100">
-
+    <form action="" class="w-100 h-100 overflow-auto position-relative d-flex flex-column">
+        <div class="w-100 h-100 overflow-auto">
+            <div class="w-100 d-flex flex-column gap-3 justify-content-center align-items-center">
+                <div class="w-100 p-4 row m-0 row-gap-5">
+                    <div class="col-lg-6 col-12 p-0 d-flex flex-column gap-3">
+                        <div>
+                            <h5>Other Personal Information</h5>
                         </div>
-                    <?php } ?>
-                </div>
-
-                <div class="position-absolute p-4 pt-5">
-
-                    <?php if ($usersDetails['profilePicture']) { ?>
-                        <img src="" alt="profilePicture"
-                            class="d-flex align-items-center justify-content-center bg-light profileImage border border-2 border-primary">
-                    <?php } else { ?>
-                        <div
-                            class="d-flex align-items-center justify-content-center bg-light profileImage border border-2 border-primary">
+                        <div class="d-flex gap-3 align-items-center">
                             <span class="material-symbols-rounded">
-                                person
+                                <?= $usersDetails['gender'] ?>
                             </span>
+                            <p>
+                                <?= $usersDetails['gender'] ?>
+                            </p>
                         </div>
-                    <?php } ?>
-                </div>
-            </div>
-        </div>
-        <div class="w-100 p-4 pt-5 border-1 border-bottom border-primary text-md-start text-center">
-            <h3>
-                <?= $usersDetails['firstName'] . ($usersDetails['middleName'] ? " " . $usersDetails['middleName'] : "") . " " . $usersDetails['lastName'] . ($usersDetails['suffix'] ? " " . $usersDetails['suffix'] : ""); ?>
-            </h3>
-        </div>
-        <div class="w-100 h-100 p-4">
-            <div class="h-100 row m-0 row-gap-5">
-                <div class="col-lg-6 col-12 p-0 d-flex flex-column gap-3">
-                    <div>
-                        <h5>Other Personal Information</h5>
+                        <div class="d-flex gap-3 align-items-center">
+                            <span class="material-symbols-rounded">
+                                event
+                            </span>
+                            <p>
+                                <?= $usersDetails['birthDate'] ?>
+                            </p>
+                        </div>
+                        <div class="d-flex gap-3 align-items-center">
+                            <span class="material-symbols-rounded">
+                                calendar_clock
+                            </span>
+                            <p>
+                                <?= $usersDetails['age'] ?> Years Old
+                            </p>
+                        </div>
                     </div>
-                    <div class="d-flex gap-3 align-items-center">
-                        <span class="material-symbols-rounded">
-                            <?= $usersDetails['gender'] ?>
-                        </span>
-                        <p>
-                            <?= $usersDetails['gender'] ?>
-                        </p>
-                    </div>
-                    <div class="d-flex gap-3 align-items-center">
-                        <span class="material-symbols-rounded">
-                            event
-                        </span>
-                        <p>
-                            <?= $usersDetails['birthDate'] ?>
-                        </p>
-                    </div>
-                    <div class="d-flex gap-3 align-items-center">
-                        <span class="material-symbols-rounded">
-                            calendar_clock
-                        </span>
-                        <p>
-                            <?= $usersDetails['age'] ?> Years Old
-                        </p>
-                    </div>
-                </div>
 
-                <div class="col-lg-6 col-12 p-0 d-flex flex-column gap-3">
-                    <div>
-                        <h5>Contact Information</h5>
-                    </div>
-                    <div class="d-flex gap-3 align-items-center">
-                        <span class="material-symbols-rounded">
-                            phone
-                        </span>
-                        <p>
-                            <?= $usersDetails['contactNumber'] ?>
-                        </p>
-                    </div>
-                    <div class="d-flex gap-3 align-items-center">
-                        <span class="material-symbols-rounded">
-                            mail
-                        </span>
-                        <p>
-                            <?= $usersDetails['emailAddress'] ?>
-                        </p>
+                    <div class="col-lg-6 col-12 p-0 d-flex flex-column gap-3">
+                        <div>
+                            <h5>Contact Information</h5>
+                        </div>
+                        <div class="d-flex gap-3 align-items-center">
+                            <span class="material-symbols-rounded">
+                                phone
+                            </span>
+                            <p>
+                                <?= $usersDetails['contactNumber'] ?>
+                            </p>
+                        </div>
+                        <div class="d-flex gap-3 align-items-center">
+                            <span class="material-symbols-rounded">
+                                mail
+                            </span>
+                            <p>
+                                <?= $usersDetails['emailAddress'] ?>
+                            </p>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
+
+        <!-- Bottom Navigations -->
+        <div class="col-12 p-4 border-top border-primary text-primary">
+            <div class="d-flex align-items-center justify-content-center justify-content-lg-end gap-3">
+                <a href="#" class="text-nowrap btn btn-primary p-3 px-5">Add an User</a>
+            </div>
+        </div>
+    </form>
 </main>
