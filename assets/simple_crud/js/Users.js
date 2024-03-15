6c5ee115bb3566b2500bb494707a5263792e38f2
Amar Maravilla <amar@clearmindai.com>
2024-03-15 20:06:40 +0800

    Simple CRUD (Updating a JS)
    Simple CRUD (Updating a JS, the Users JS)

--- assets/simple_crud/js/Users.js
+++ assets/simple_crud/js/Users.js
@@ -30,10 +30,10 @@ const usersFunctions = (() => {
 						try {
 							vPPResponse = response ? JSON.parse(response) : "";
 
-							if (vPPResponse !== null && vPPResponse !== undefined) {
+							if (vPPResponse) {
 								if (typeof vPPResponse === `object` || typeof vPPResponse === `array`) {
 									$(`.modalLoader`).fadeOut(`slow`);
-	
+
 									setTimeout(function () {
 										// Modal Header
 										if (vPPResponse.modalHeader !== undefined) {
@@ -41,29 +41,52 @@ const usersFunctions = (() => {
 											$(`.modal-title`).find(`span`).text(vPPResponse.modalHeader.titleLogo);
 											$(`.modal-title`).find(`h5`).text(vPPResponse.modalHeader.title);
 										}
-	
+
 										// Modal body
 										if (vPPResponse.modalBody !== undefined) {
 											$(`.modal-body`).append(vPPResponse.modalBody);
 										}
-	
+										else {
+											$(`.modal-header .btn-close`).trigger(`click`);
+
+											globalFunctions.toast(`danger`, `Empty Response.`, `error`);
+										}
+
 										// Modal Footer
 										if (vPPResponse.modalFooter !== undefined) {
 											$(`.modal-footer`).removeClass(`d-none`);
 										}
 									}, 1000);
-	
+
 									console.log(`File: Users.js => vPPResponse: `, vPPResponse);
 								}
 								else {
 									console.log(typeof vPPResponse);
+
+									setTimeout(function () {
+										$(`.modal-header .btn-close`).trigger(`click`);
+
+										globalFunctions.toast(`danger`, `Data Type of the Response is not an Object or an Array. Response Data Type: ${typeof vPPResponse}`, `error`);
+									}, 1000);
 								}
 							}
 							else {
 								console.log(`No Response`);
+
+								setTimeout(function () {
+									$(`.modal-header .btn-close`).trigger(`click`);
+
+									globalFunctions.toast(`danger`, `No Response.`, `error`);
+								}, 1000);
 							}
 						} catch (error) {
 							console.log(error);
+
+							setTimeout(function () {
+								$(`.modal-header .btn-close`).trigger(`click`);
+
+								globalFunctions.toast(`danger`, `${error}`, `error`);
+							}, 1000);
 						}
 					},
 					error: function (xhr, status, error) {
