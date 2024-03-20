909c4f153785c2f8c4fb247612a7e2a6e4d773d3
Amar Maravilla <amar@clearmindai.com>
2024-03-20 23:00:37 +0800

    Simple CRUD (Updating a Config)
    Simple CRUD (Updating a Config, the routes.php)

--- application/config/routes.php
+++ application/config/routes.php
@@ -1,66 +1,109 @@
 <?php
-    defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-    $route['default_controller'] = 'Welcome';
-    $route['404_override'] = '';
-    $route['translate_uri_dashes'] = FALSE;
+/*
+| -------------------------------------------------------------------------
+| URI ROUTING
+| -------------------------------------------------------------------------
+| This file lets you re-map URI requests to specific controller functions.
+|
+| Typically there is a one-to-one relationship between a URL string
+| and its corresponding controller class/method. The segments in a
+| URL normally follow this pattern:
+|
+|	example.com/class/method/id/
+|
+| In some instances, however, you may want to remap this relationship
+| so that a different class/function is called than the one
+| corresponding to the URL.
+|
+| Please see the user guide for complete details:
+|
+|	http://codeigniter.com/user_guide/general/routing.html
+|
+| -------------------------------------------------------------------------
+| RESERVED ROUTES
+| -------------------------------------------------------------------------
+|
+| There area two reserved routes:
+|
+|	$route['default_controller'] = 'welcome';
+|
+| This route indicates which controller class should be loaded if the
+| URI contains no data. In the above example, the "welcome" class
+| would be loaded.
+|
+|	$route['404_override'] = 'errors/page_missing';
+|
+| This route will tell the Router what URI segments to use if those provided
+| in the URL cannot be matched to a valid route.
+|
+*/
 
-    // Added by Amar Maravilla
-    // Welcome Routes
-    $route['about'] = 'Welcome/about';
+$route['default_controller'] = 'Welcome';
+$route['404_override'] = '';
+$route['translate_uri_dashes'] = FALSE;
 
-    // PageController Routes
-    $route['home'] = 'PageController';
-    $route['about_us'] = 'PageController/aboutus';
-    $route['blog/(:any)'] = 'PageController/blog/$1';
-    $route['access_denied']['GET'] = 'PageController/accessDenied';
+$route['cmai/pay1st']['GET'] = 'cmai/Pay1st';
+$route['cmai/pay1st']['POST'] = 'cmai/Pay1st';
 
-    // Register Routes
-    $route['register']['GET'] = 'auth/RegisterController';
-    $route['register']['POST'] = 'auth/RegisterController/register';
+// Added by Amar Maravilla
+// Welcome Routes
+$route['about'] = 'Welcome/about';
 
-    // Login Routes
-    $route['login']['GET'] = 'auth/LoginController';
-    $route['login']['POST'] = 'auth/LoginController/login';
+// PageController Routes
+$route['home'] = 'PageController';
+$route['about_us'] = 'PageController/aboutus';
+$route['blog/(:any)'] = 'PageController/blog/$1';
+$route['access_denied']['GET'] = 'PageController/accessDenied';
 
-    // Logout Routes
-    $route['logout']['GET'] = 'auth/LogoutController/logout';
+// Register Routes
+$route['register']['GET'] = 'auth/RegisterController';
+$route['register']['POST'] = 'auth/RegisterController/register';
 
-    // Users Routes
-    $route['users']['GET'] = 'users/UsersController';
+// Login Routes
+$route['login']['GET'] = 'auth/LoginController';
+$route['login']['POST'] = 'auth/LoginController/login';
 
-    // Admin Routes
-    $route['admins']['GET'] = 'admins/AdminsController';
+// Logout Routes
+$route['logout']['GET'] = 'auth/LogoutController/logout';
 
-    // Employee Routes
-    $route['employee'] = 'employee/EmployeeController';
-    $route['employee/create'] = 'employee/EmployeeController/create';
-    $route['employee/store'] = 'employee/EmployeeController/store';
-    $route['employee/edit/(:num)'] = 'employee/EmployeeController/edit/$1';
-    $route['employee/update/(:num)'] = 'employee/EmployeeController/update/$1';
-    $route['employee/delete/(:num)'] = 'employee/EmployeeController/delete/$1';
-    $route['employee/confirmdelete/(:num)']['DELETE'] = 'employee/EmployeeController/delete/$1';
+// Users Routes
+$route['users']['GET'] = 'users/UsersController';
 
-    // Simple CRUD Routes
-    // Login Controller Routes
-    $route['simple_crud/login']['GET'] = 'simple_crud/authenticate/Login';
-    $route['simple_crud/login']['POST'] = 'simple_crud/authenticate/Login/login';
+// Admin Routes
+$route['admins']['GET'] = 'admins/AdminsController';
 
-    // Logout Controller Routes
-    $route['simple_crud/logout']['GET'] = 'simple_crud/authenticate/Logout/logout';
+// Employee Routes
+$route['employee'] = 'employee/EmployeeController';
+$route['employee/create'] = 'employee/EmployeeController/create';
+$route['employee/store'] = 'employee/EmployeeController/store';
+$route['employee/edit/(:num)'] = 'employee/EmployeeController/edit/$1';
+$route['employee/update/(:num)'] = 'employee/EmployeeController/update/$1';
+$route['employee/delete/(:num)'] = 'employee/EmployeeController/delete/$1';
+$route['employee/confirmdelete/(:num)']['DELETE'] = 'employee/EmployeeController/delete/$1';
 
-    // Register Controller Routes
-    $route['simple_crud/register']['GET'] = 'simple_crud/authenticate/Register';
-    $route['simple_crud/register']['POST'] = 'simple_crud/authenticate/Register/register';
+// Simple CRUD Routes
+// Login Controller Routes
+$route['simple_crud/login']['GET'] = 'simple_crud/authenticate/Login';
+$route['simple_crud/login']['POST'] = 'simple_crud/authenticate/Login/login';
 
-    // Users Controller Routes
-    $route['simple_crud/users']['GET'] = 'simple_crud/users/Users';
-    $route['simple_crud/users/users_list']['GET'] = 'simple_crud/users/Users/usersList';
-    $route['simple_crud/users/my_profile']['GET'] = 'simple_crud/users/Users/myProfile';
-    $route['simple_crud/users/edit_my_profile']['GET'] = 'simple_crud/users/Users/editMyProfile';
-    $route['simple_crud/users/edit_my_profile']['POST'] = 'simple_crud/users/Users/editMyProfile';
-    $route['simple_crud/users/change_password']['GET'] = 'simple_crud/users/Users/changePassword';
-    $route['simple_crud/users/change_password']['POST'] = 'simple_crud/users/Users/changePassword';
+// Logout Controller Routes
+$route['simple_crud/logout']['GET'] = 'simple_crud/authenticate/Logout/logout';
 
-    // Random Seeder Controller Routes
-    $route['simple_crud/random_seeder']['GET'] = 'simple_crud/seeder/RandomSeeder/seed';
+// Register Controller Routes
+$route['simple_crud/register']['GET'] = 'simple_crud/authenticate/Register';
+$route['simple_crud/register']['POST'] = 'simple_crud/authenticate/Register/register';
+
+// Users Controller Routes
+$route['simple_crud/users']['GET'] = 'simple_crud/users/Users';
+$route['simple_crud/users/users_list']['GET'] = 'simple_crud/users/Users/usersList';
+$route['simple_crud/users/my_profile']['GET'] = 'simple_crud/users/Users/myProfile';
+$route['simple_crud/users/my_profile/view_profile_picture']['GET'] = 'simple_crud/users/Users/viewProfilePicture';
+$route['simple_crud/users/edit_my_profile']['GET'] = 'simple_crud/users/Users/editMyProfile';
+$route['simple_crud/users/edit_my_profile']['POST'] = 'simple_crud/users/Users/editMyProfile';
+$route['simple_crud/users/change_password']['GET'] = 'simple_crud/users/Users/changePassword';
+$route['simple_crud/users/change_password']['POST'] = 'simple_crud/users/Users/changePassword';
+
+// Random Seeder Controller Routes
+$route['simple_crud/random_seeder']['GET'] = 'simple_crud/seeder/RandomSeeder/seed';
